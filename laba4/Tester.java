c39eb35b1dd4409ba0604f4c93ac9ad790bf5141
anassulima <56161429+anassulima@users.noreply.github.com>
2020-01-17 04:27:31 +0300

    Update and rename CarTest.java to Tester.java

---
+++ laba4/Tester.java
@@ -0,0 +1,10 @@
+public class Tester {
+    public static void main(String[] args) {
+        Car c1 = new Car("Dutsun", "Ondo", "Trust", 280000, 2012);
+        Car c2 = new Car("BMW", "X6", "E71", 3500000, 2014);
+        Car c3 = new Car("Shkoda", "Superb", "Standart", 750000, 2013);
+        c1.println();
+        c2.println();
+        c3.println();
+    }
+}
